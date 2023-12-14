22d8e0205bac596a6a3c2f1c8eee28d54cd69c30
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-14 09:35:12 +0300

    Arrow functions

---
+++ index.js
@@ -0,0 +1,21 @@
+const divide = (num1, num2) => {
+    return 2000 / 100;
+}
+divide();
+const result = divide()
+console.log(result);
+
+
+const square = (a) => {
+    return 2 * 2;
+}
+square();
+const sqre = square();
+console.log(sqre);
+
+const add = (x, y) => {
+    return 3 + 4;
+}
+add();
+const sum = add();
+console.log(sum);
